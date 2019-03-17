65d5ad6f8b8430f12c121bb349d4911640ce32a2
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-03-16 23:08:56 -0500

    Socket io and sigleton pattern

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ var server_1 = __importDefault(require("./classes/server"));
 var routes_1 = __importDefault(require("./routes/routes"));
 var bodyParser = require("body-parser");
 var cors_1 = __importDefault(require("cors"));
-var server = new server_1.default();
+var server = server_1.default.instance;
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
 server.app.use(cors_1.default({ origin: true, credentials: true }));
